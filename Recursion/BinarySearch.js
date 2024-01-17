44cd7f27d3d9d9a5bdc41fa209cd3d46a8dcd45d
ashishbadhe97 <ashishbadhe97@gmail.com>
2024-01-17 09:50:37 -0500

    BS Recursion

--- Recursion/BinarySearch.js
+++ Recursion/BinarySearch.js
@@ -1,9 +1,31 @@
 /**
- * 704. Binary Search
+ * 704. Binary Search (by Recursion)
  * 
  * https://leetcode.com/problems/binary-search/description/
  */
 
 
+var search = function(nums, target) {
+    return recursiveBinarySearch(0, nums.length - 1, nums, target);
+};
 
+const recursiveBinarySearch = (start, end, nums, target) => {
 
+    if(start > end){
+        return -1;
+    }
+
+    let mid = Math.trunc(start + (end - start) / 2);
+
+    if(nums[mid] === target){
+        return mid;
+    }
+
+    if(nums[mid] > target){
+        return recursiveBinarySearch(start, mid - 1, nums, target);
+    }else{
+        return recursiveBinarySearch(mid + 1, end, nums, target);
+    }
+}
+
+console.log(search([-1,0,3,5,9,12], 9))
